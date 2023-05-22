1f01d6eac8d2e65731b19f9e8bf6e90f9650a8df
vinoja98 <vinoja.18@cse.mrt.ac.lk>
2023-05-22 11:53:01 +0530

    Add changes

--- src/main/java/com/location/router/Controller.java
+++ src/main/java/com/location/router/Controller.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
-    private HttpServletRequest request;
-    private HttpServletResponse response;
+    private final HttpServletRequest request;
+    private final HttpServletResponse response;
 
 
     public Controller(HttpServletRequest request,HttpServletResponse response){
